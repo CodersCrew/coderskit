8ca8798dece741089ec8b0a7886b0a3c447886dd
Konrad Szwarc <konrad.szwarc@outlook.com>
2019-07-24 23:57:58 +0200

    Initial commit

---
+++ packages/docs/src/atoms/Button/Button.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { select, boolean } from '@storybook/addon-knobs';
+import { Button, colors as themeColors } from 'coderskit';
+import readme from './Button.md';
+
+const variants = {
+  contained: 'contained',
+  outlined: 'outlined',
+  text: 'text',
+};
+
+const sizes = {
+  large: 'large',
+  default: 'default',
+  small: 'small',
+};
+
+const elements = {
+  a: 'a',
+  button: 'button',
+  div: 'div',
+  span: 'span',
+};
+
+const kinds = Object.keys(themeColors).reduce((a, key) => ({ ...a, [key]: key }), {});
+
+storiesOf('Atoms', module)
+  .addParameters({
+    readme: {
+      content: readme,
+    },
+  })
+  .add('Button', () => {
+    const props = {
+      disabled: boolean('disabled', false),
+      variant: select('variant', variants, 'contained') as keyof typeof variants,
+      kind: select('kind', kinds, 'primary') as keyof typeof kinds,
+      size: select('size', sizes, 'default') as keyof typeof sizes,
+      as: select('as', elements, 'button') as keyof typeof elements,
+    };
+
+    return <Button {...props}>Example Button</Button>;
+  });
