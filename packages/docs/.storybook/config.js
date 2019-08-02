a5b76ce4546f020216918f6b3fc4970325c5d823
Konrad Szwarc <konrad.szwarc@outlook.com>
2019-08-02 10:26:33 +0200

    Change scripts for building storybook

--- packages/docs/.storybook/config.js
+++ packages/docs/.storybook/config.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { configure, addDecorator } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
 import { ThemeProvider } from 'emotion-theming';
 import { Global } from '@emotion/core';
 import { addReadme } from 'storybook-readme';
 import { theme, globalStyle, initializeNotifications } from 'coderskit';
+
+addParameters({
+  options: {
+    panelPosition: 'right',
+  },
+});
 
 const req = require.context('../src', true, /.stories.tsx$/);
 
