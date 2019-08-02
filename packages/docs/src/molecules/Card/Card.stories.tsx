8f2cc60ce91d6f21f126d404a08fa52ce43fb3ca
Hanna Domagała <hanna-domagala@coderscrew.pl>
2019-08-02 16:09:27 +0200

    Create Card storybook

---
+++ packages/docs/src/molecules/Card/Card.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { boolean, text, select, number } from '@storybook/addon-knobs';
+import { Card, Typography, Button, Icon } from 'coderskit';
+
+const widthRange = {
+  range: true,
+  min: 280,
+  max: 450,
+  step: 10,
+};
+
+const elements = {
+  div: 'div',
+  article: 'article',
+};
+
+storiesOf('Molecules', module).add('Card', () => {
+  const props = {
+    image: text('image', 'popcorn.jpg'),
+    width: number('width', 280, widthRange),
+    inlineBtn: boolean('inline buttons', false),
+    as: select('as', elements, 'div') as keyof typeof elements,
+  };
+
+  const data = {
+    header: text('header', 'Card title'),
+    content: text(
+      'content',
+      'Completely synergize resource taxing relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas.',
+    ),
+  };
+
+  return (
+    <Card {...props}>
+      <Typography el="h3">{data.header}</Typography>
+      <Card.Content>
+        <Typography el="body2">{data.content}</Typography>
+      </Card.Content>
+      <Card.Footer>
+        <Button variant="outlined" color="gray">
+          <Icon src="edit-solid.svg" />
+        </Button>
+        <Button>Details</Button>
+      </Card.Footer>
+    </Card>
+  );
+});
