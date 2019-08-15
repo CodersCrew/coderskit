3d87ec17159db2c0c285bd2e933385755d13d727
Bartosz Radoś <32978631+radosbartosz@users.noreply.github.com>
2019-08-15 23:49:04 +0200

    feature/breadcrumb (#7)
    * Create Breadcrumb component
    
    * Change props descriptions
    
    * Fix double event

---
+++ packages/docs/src/atoms/Breadcrumb/Breadcrumb.stories.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { boolean, select, number, text } from '@storybook/addon-knobs';
+import { Breadcrumb, colors as themeColors } from 'coderskit';
+import readme from './Breadcrumb.md';
+
+const colors = Object.keys(themeColors).reduce((a, key) => ({ ...a, [key]: key }), {});
+
+const icons = {
+  user: 'user-solid.svg',
+};
+
+const separators = {
+  slash: 'slash-solid.svg',
+  arrow: 'arrow-right-solid.svg',
+  chevron: 'chevron-right-solid.svg',
+};
+
+storiesOf('Atoms', module)
+  .addParameters({
+    readme: {
+      content: readme,
+    },
+  })
+  .add('Breadcrumb', () => {
+    const props = {
+      name: text('name', 'Item 1'),
+      color: select('color', colors, 'fontPlaceholder') as keyof typeof colors,
+      size: number('size', 14),
+      src: select('src', icons, icons.user) as keyof typeof icons,
+      separator: select('separator', separators, separators.slash) as keyof typeof separators,
+      isIconVisible: boolean('iconVisible', true),
+      isDividerVisible: boolean('dividerVisible', true),
+      href: text('href', 'javascript:;'),
+    };
+
+    return (
+      <div style={{ display: 'flex' }}>
+        <Breadcrumb {...props} />
+        <Breadcrumb {...props} />
+        <Breadcrumb {...props} isDividerVisible={false} />
+      </div>
+    );
+  });
